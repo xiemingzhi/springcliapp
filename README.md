f5152a63b84239fcf4e060aa3393e4fc9e3ef00e
ming <xiemingzhi@gmail.com>
2020-08-07 13:01:23 +0800

    Spring Kafka Example
    1. docker run kafka
    2. create topic
    3. start consumer for testing
    4. mvn spring-boot:run
    spring boot configurableapplicationcontext example

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
+# Prerequisites 
+
+Docker run kafka 
+
+Create topic 
+```
+sudo docker run -it --rm \
+    --network kafka_default \
+    -e KAFKA_CFG_ZOOKEEPER_CONNECT=kafka_zookeeper_1:2181 \
+    bitnami/kafka:2 kafka-topics.sh --create --zookeeper kafka_zookeeper_1:2181 --replication-factor 1  --partitions 1 --topic general
+```
+
+List topics 
+```
+sudo docker run -it --rm \
+    --network kafka_default \
+    -e KAFKA_CFG_ZOOKEEPER_CONNECT=kafka_zookeeper_1:2181 \
+    bitnami/kafka:2 kafka-topics.sh --list --zookeeper kafka_zookeeper_1:2181
+```
+
+Start consumers(for debugging)
+```
+sudo docker run -it --rm \
+    --network kafka_default \
+    -e KAFKA_CFG_ZOOKEEPER_CONNECT=kafka_zookeeper_1:2181 \
+    bitnami/kafka:2 kafka-console-consumer.sh --bootstrap-server kafka_kafka_1:9092 --topic general --from-beginning
+```
+
 # Build
 ```
 mvn package
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,107 @@
 package com.example.demo;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+
+		MessageProducer producer = context.getBean(MessageProducer.class);
+		MessageListener listener = context.getBean(MessageListener.class);
+		/*
+		 * Sending a Hello World message to topic 'baeldung'. 
+		 * Must be recieved by both listeners with group foo
+		 * and bar with containerFactory fooKafkaListenerContainerFactory
+		 * and barKafkaListenerContainerFactory respectively.
+		 * It will also be recieved by the listener with
+		 * headersKafkaListenerContainerFactory as container factory
+		 */
+		producer.sendMessage("Hello, World!");
+		listener.latch.await(10, TimeUnit.SECONDS);
+
+		//SpringApplication.run(DemoApplication.class, args);
 		System.out.println("finished startup");
+		context.close();
 	}
 
+	@Bean
+    public MessageProducer messageProducer() {
+        return new MessageProducer();
+    }
+
+    @Bean
+    public MessageListener messageListener() {
+        return new MessageListener();
+    }
+
+    public static class MessageProducer {
+
+        @Autowired
+        private KafkaTemplate<String, String> kafkaTemplate;
+
+        @Value(value = "${message.topic.name}")
+        private String topicName;
+
+        public void sendMessage(String message) {
+
+            ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topicName, message);
+
+            future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+                @Override
+                public void onSuccess(SendResult<String, String> result) {
+                    System.out.println("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata()
+                        .offset() + "]");
+                }
+
+                @Override
+                public void onFailure(Throwable ex) {
+                    System.out.println("Unable to send message=[" + message + "] due to : " + ex.getMessage());
+                }
+            });
+        }
+
+    }
+
+    public static class MessageListener {
+
+        private CountDownLatch latch = new CountDownLatch(3);
+
+        @KafkaListener(topics = "${message.topic.name}", groupId = "foo", containerFactory = "fooKafkaListenerContainerFactory")
+        public void listenGroupFoo(String message) {
+            System.out.println("Received Messasge in group 'foo': " + message);
+            latch.countDown();
+        }
+
+        @KafkaListener(topics = "${message.topic.name}", groupId = "bar", containerFactory = "barKafkaListenerContainerFactory")
+        public void listenGroupBar(String message) {
+            System.out.println("Received Messasge in group 'bar': " + message);
+            latch.countDown();
+        }
+
+        @KafkaListener(topics = "${message.topic.name}", containerFactory = "headersKafkaListenerContainerFactory")
+        public void listenWithHeaders(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+            System.out.println("Received Messasge: " + message + " from partition: " + partition);
+            latch.countDown();
+        }
+
+    }
 }
